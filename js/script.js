69aa750c5e2c3e566c9c06b7d0b2ecab8ac3b75a
Ricardo Andrade <ricardo.maia@ccc.ufcg.edu.br>
2021-07-18 21:39:57 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const carregando = `<p>Carregando...</p>`;
+    const concluido = '<p>Cadastrado sucesso! =)</p>';
+    //adiquirindo dados
+    const formCotainer = document.getElementById('cotainer-singup');
+    const userName = document.getElementById('name');
+    const email = document.getElementById('email');
+    console.log(userName);
+    console.log(email);
+
+    const user = {
+        nome:userName.value,
+        email:email.value
+    }
+
+    // mudando tela para carregando
+    formCotainer.innerHTML = carregando;
+
+    localStorage.setItem('lead', JSON.stringify(user));
+
+    // mudando tela para sucesso
+
+    formCotainer.innerHTML = concluido;
+});
